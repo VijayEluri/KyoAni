b3c9a2d54eaca99df0508732db4939ee55ce1bdd
Yamada Masaki <masaki@hisme.net>
2009-07-21 02:08:01 +0900

    プロジェクト開始

---
+++ src/net/hisme/masaki/kyoani/KyouAni.java
@@ -0,0 +1,13 @@
+package net.hisme.masaki.kyoani;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class KyouAni extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
