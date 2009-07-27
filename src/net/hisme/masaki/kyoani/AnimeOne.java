44861cb5cb22544cd5917a4296090648f34f895b
Yamada Masaki <masaki@hisme.net>
2009-07-28 05:48:54 +0900

    Apacheのhttpクライアントライブラリを使ってログインを実装

--- src/net/hisme/masaki/kyoani/AnimeOne.java
+++ src/net/hisme/masaki/kyoani/AnimeOne.java
@@ -1,137 +1,66 @@
 package net.hisme.masaki.kyoani;
 
-import android.content.Context;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.HttpResponse;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.cookie.Cookie;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import javax.net.ssl.HttpsURLConnection;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import android.util.Log;
-import java.util.Map;
-import java.util.List;
-import java.util.Hashtable;
 
 public class AnimeOne {
-	private Context context = null;
-	private Account account = null;
-	private String user_hash = null;
-	private Hashtable<String, String> cookie = null;
+	Account account = null;
+	DefaultHttpClient http;
 	private static final String MYPAGE_URI = "http://anime.biglobe.ne.jp/program/myprogram";
 	private static final String LOGIN_FORM = "https://anime.biglobe.ne.jp/login";
 	private static final String LOGIN_URI = "https://anime.biglobe.ne.jp/login/login_ajax";
-	private static final String COOKIE_FILE = "cookie.txt";
 
-	public AnimeOne(Context context, Account account) {
-		this.context = context;
+	public AnimeOne(Account account) {
 		this.account = account;
-		this.cookie = new Hashtable<String, String>();
-		loadCookie();
+		this.http = new DefaultHttpClient();
 	}
 
 	public void mypage() {
-
+		HttpGet get = new HttpGet(MYPAGE_URI);
 	}
 
-	private void loadCookie() {
-		try {
-			String line = null;
-			BufferedReader reader = new BufferedReader(new InputStreamReader(
-					this.context.openFileInput(COOKIE_FILE)));
-			while ((line = reader.readLine()) != null) {
-				String[] hash = line.split("=");
-				this.cookie.put(hash[0], hash[1]);
-			}
-			reader.close();
-		} catch (java.io.FileNotFoundException e) {
-		} catch (java.io.IOException e) {
-		}
-	}
-
-	private void saveCookie() {
-		try {
-			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
-					this.context.openFileOutput(COOKIE_FILE, Context.MODE_PRIVATE)));
-			for (Map.Entry<String, String> entry : this.cookie.entrySet()) {
-				writer.write(entry.getKey() + "=" + entry.getValue() + "\n");
-			}
-			writer.flush();
-			writer.close();
-		} catch (java.io.FileNotFoundException e) {
-		} catch (java.io.IOException e) {
-		}
-	}
-
-	private String request(HttpURLConnection http, String method, String message) {
-		String responseText = "";
-		try {
-			http.setRequestMethod(method);
-			http.setDoInput(true);
-			http.setDoOutput(true);
-			String cookie_str = "";
-			for (Map.Entry<String, String> cookie_entry : this.cookie.entrySet()) {
-				cookie_str += cookie_entry.getKey() + "=" + cookie_entry.getValue()
-						+ "; ";
-			}
-			Log.d("AnimeOne", "Cookie: " + cookie_str);
-			http.addRequestProperty("Cookie", cookie_str);
-			if (method.equals("POST")) {
-				PrintWriter writer = new PrintWriter(http.getOutputStream());
-				Log.d("AnimeOne", "message: " + message);
-				writer.print(message);
-				writer.flush();
-				writer.close();
-			} else {
-				http.connect();
-			}
-			BufferedReader reader = new BufferedReader(new InputStreamReader(http
-					.getInputStream()));
-			String line = null;
-			while ((line = reader.readLine()) != null) {
-				responseText += line + "\n";
-			}
-			Log.d("AnimeOne", "RESPONSE: " + responseText);
-			reader.close();
-
-			List<String> set_cookies = http.getHeaderFields().get("set-cookie");
-
-			if (set_cookies != null) {
-				Log.d("AnimeOne", "Set-Cookie: " + set_cookies.toString());
-				for (String set_cookie : set_cookies) {
-					String[] hash = set_cookie.split("[=;]");
-					this.cookie.put(hash[0], hash[1]);
-				}
-				saveCookie();
-			}
-		} catch (java.net.ProtocolException e) {
-		} catch (java.io.IOException e) {
-		}
-		return responseText;
+	private boolean isLogined() {
+		boolean result = false;
+		return result;
 	}
 
 	public boolean login() {
-		String user_id = account.getUser();
-		String password = account.getPassword();
-		String message = "mail=" + user_id + "&password=" + password;
 		boolean result = false;
-		HttpsURLConnection http = null;
 		try {
-			/*
-			 * http = (HttpsURLConnection) new URL(LOGIN_FORM) .openConnection();
-			 * request(http, "GET", null); http.disconnect();
-			 */
-			http = (HttpsURLConnection) new URL(LOGIN_URI).openConnection();
-			http.addRequestProperty("Referer", LOGIN_FORM);
-			request(http, "POST", message);
-			http.disconnect();
+			HttpGet get = new HttpGet(LOGIN_FORM);
+			http.execute(get);
 
+			String user_id = account.getUser();
+			String password = account.getPassword();
+			HttpPost post = new HttpPost(LOGIN_URI);
+			post.addHeader("Content-Type", "application/x-www-form-urlencoded");
+			post.addHeader("Referer", LOGIN_FORM);
+			post.setEntity(new StringEntity("mail=" + user_id + "&password="
+					+ password));
+			http.execute(post);
+
+			for (Cookie cookie : http.getCookieStore().getCookies()) {
+				if (cookie.getName().equals("user[id_nick]")) {
+					result = true;
+					break;
+				}
+			}
+		} catch (org.apache.http.client.ClientProtocolException e) {
+			log(e.toString());
 		} catch (java.io.IOException e) {
-
-		} catch (Exception e) {
-			Log.d("AnimeOne.login", e.toString());
+			log(e.toString());
 		}
+		log("result = " + result);
 		return result;
+	}
+
+	private void log(String str) {
+		Log.d("AnimeOne", str);
 	}
 }
