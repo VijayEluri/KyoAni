0d1a83a37e7483510c4fb1c50961f1a3cf731d2a
Yamada Masaki <masaki@hisme.net>
2009-08-31 07:54:52 +0900

    いらないimport削除

--- src/net/hisme/masaki/kyoani/KyoAni.java
+++ src/net/hisme/masaki/kyoani/KyoAni.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
-import android.content.Context;
 import android.net.Uri;
 
 public class KyoAni extends Activity {
