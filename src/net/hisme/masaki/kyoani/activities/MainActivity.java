7208874260769a524e7c62005200f3be1ef4153e
Yamada Masaki <masaki@hisme.net>
2010-02-12 07:15:43 +0900

    メインアクティビティとスケジュールリストアクティビティを追加

---
+++ src/net/hisme/masaki/kyoani/activities/MainActivity.java
@@ -0,0 +1,18 @@
+package net.hisme.masaki.kyoani.activities;
+
+import net.hisme.masaki.kyoani.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import android.net.Uri;
+
+public class MainActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
