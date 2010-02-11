01abbca2399b2798943fdbc87072c269df1575b6
Yamada Masaki <masaki@hisme.net>
2010-02-12 07:17:31 +0900

    widget押したらメイン画面が起動するようにした

--- src/net/hisme/masaki/kyoani/widget/KyoAniWidget.java
+++ src/net/hisme/masaki/kyoani/widget/KyoAniWidget.java
@@ -8,11 +8,14 @@ import android.content.Intent;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.ComponentName;
+import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+
+import net.hisme.masaki.kyoani.activities.MainActivity;
 
 import net.hisme.masaki.kyoani.*;
 
@@ -24,57 +27,60 @@ public class KyoAniWidget extends AppWidgetProvider {
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
 			int[] appWidgetIds) {
 		super.onUpdate(context, appWidgetManager, appWidgetIds);
-		updateAppWidget(context);
-		log("CallUpdate from onUpdate");
+		
+		Intent intent = new Intent(context, MainActivity.class);
+		for (int i = 0; i < appWidgetIds.length; i++) {
+			int widgetId = appWidgetIds[i];
+			PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
+					intent, 0);
+			RemoteViews _views = new RemoteViews(context.getPackageName(),
+					R.layout.widget_layout);
+			_views.setOnClickPendingIntent(R.id.next_log, pendingIntent);
+			_views.setOnClickPendingIntent(R.id.widget_main, pendingIntent);
+			appWidgetManager.updateAppWidget(widgetId, _views);
+			updateAppWidget(context, appWidgetManager, _views);
+		}
 	}
 
-	public void onReceive(Context context, Intent intent) {
-		super.onReceive(context, intent);
-		Log.d("KyoAni", intent.toString());
-		// if (intent.getAction().equals(AppWidgetManager.ACTION_APPWIDGET_UPDATE))
-		// {
-		// updateAppWidget(context);
-		// log("CallUpdate from onReceive");
-		// } else {
-		// super.onReceive(context, intent);
-		// }
-	}
-
-	public static void updateAppWidget(Context _context) {
+	public static void updateAppWidget(Context _context,
+			AppWidgetManager _manager, RemoteViews _views) {
 		context = _context;
-		manager = AppWidgetManager.getInstance(context);
+		manager = _manager;
+		views = _views;
 
 		SharedPreferences pref = PreferenceManager
 				.getDefaultSharedPreferences(context);
-		views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
 
 		final String account = pref.getString("account", "");
 		final String password = pref.getString("password", "");
 
 		Thread thread = new Thread(new Runnable() {
 			public void run() {
-				AnimeOne anime_one = new AnimeOne(new Account(account, password));
-				String schedule_str = null;
-				schedule_str = context.getText(R.string.login_failure).toString();
+				AnimeOne anime_one = new AnimeOne(
+						new Account(account, password));
+				StringBuffer schedule_str = new StringBuffer();
+				schedule_str.append(context.getText(R.string.login_failure)
+						.toString());
 				log("getSchedules");
 				ArrayList<Schedule> schedules = anime_one.getSchedules(context);
 				if (schedules != null) {
-					schedule_str = context.getText(R.string.no_schedule).toString();
+					schedule_str.append(context.getText(R.string.no_schedule)
+							.toString());
 					for (Schedule schedule : schedules) {
 						GregorianCalendar now = new GregorianCalendar();
 
 						if (now.compareTo(schedule.getStart()) == -1) {
-							schedule_str = schedule.getChannel() + "\n"
-									+ schedule.getStartString() + "\n" + schedule.getName()
-									+ "\n";
+							schedule_str.append(schedule.getChannel() + "\n"
+									+ schedule.getStartString() + "\n"
+									+ schedule.getName() + "\n");
 							break;
 						}
 					}
 				}
 				views.setTextViewText(R.id.next_log, schedule_str);
 				log("Update Widget View");
-				manager.updateAppWidget(new ComponentName(context, KyoAniWidget.class),
-						views);
+				manager.updateAppWidget(new ComponentName(context,
+						KyoAniWidget.class), views);
 			}
 		});
 		log("Thread Start");
