c199d76b79f9851083130d0724e4fbeab2ec5efa
Yamada Masaki <masaki@hisme.net>
2010-02-13 06:18:15 +0900

    account設定をListActivity使う設定に

--- src/net/hisme/masaki/kyoani/activities/AccountActivity.java
+++ src/net/hisme/masaki/kyoani/activities/AccountActivity.java
@@ -2,25 +2,43 @@ package net.hisme.masaki.kyoani.activities;
 
 import net.hisme.masaki.kyoani.R;
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.ArrayAdapter;
 import android.content.Intent;
 import android.net.Uri;
 
-public class AccountActivity extends Activity {
+public class AccountActivity extends ListActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.account);
-		TextView account = (TextView) findViewById(R.id.label_account);
-		account.setOnClickListener(new View.OnClickListener() {
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, new String[] {
+						getString(R.string.label_account_setting),
+						getString(R.string.label_account_test) });
+		//setContentView(R.layout.account);
+		setListAdapter(adapter);
+		/*
+		TextView account_setting = (TextView) findViewById(R.id.account_setting);
+		account_setting.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View view) {
 				Intent intent = new Intent(AccountActivity.this,
 						SettingActivity.class);
 				startActivity(intent);
 			}
 		});
+
+		Button account_test = (Button) findViewById(R.id.account_test);
+		account_test.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+				// TODO Auto-generated method stub
+			}
+		});
+		*/
 	}
 }
