7e826344df854e6d6615e1e62bf2199b69b7e722
Yamada Masaki <masaki@hisme.net>
2010-02-13 06:20:07 +0900

    aboutのアクティビティ追加

---
+++ src/net/hisme/masaki/kyoani/activities/AboutActivity.java
@@ -0,0 +1,13 @@
+package net.hisme.masaki.kyoani.activities;
+
+import net.hisme.masaki.kyoani.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+	}
+}
