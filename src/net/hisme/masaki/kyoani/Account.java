56fb8d80d03a85bd11a13abba044346e301e57d4
Yamada Masaki <masaki@hisme.net>
2010-02-14 08:10:59 +0900

    Accountを自動ロードできるようにした

--- src/net/hisme/masaki/kyoani/Account.java
+++ src/net/hisme/masaki/kyoani/Account.java
@@ -1,8 +1,25 @@
 package net.hisme.masaki.kyoani;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
 public class Account {
 	private String user_id = null;
 	private String password = null;
+
+	public static Account load(Context context) throws BlankException {
+		SharedPreferences pref = PreferenceManager
+				.getDefaultSharedPreferences(context);
+		String user_id = pref.getString("account", null);
+		String password = pref.getString("password", null);
+		if (user_id == null || password == null || user_id == ""
+				|| password == "") {
+			throw new BlankException();
+			// return null;
+		}
+		return new Account(user_id, password);
+	}
 
 	public Account(String user_id, String password) {
 		this.user_id = user_id;
@@ -17,4 +34,6 @@ public class Account {
 		return this.password;
 	}
 
+	public static class BlankException extends Exception {
+	}
 }
