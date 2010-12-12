cf7d070632ae5957a20a84eb42a1f2e7f1f2c0e7
Yamada Masaki <masaki@hisme.net>
2010-12-13 03:43:22 +0900

    Account.load to be instance method

--- src/net/hisme/masaki/kyoani/models/Account.java
+++ src/net/hisme/masaki/kyoani/models/Account.java
@@ -13,45 +13,59 @@ import android.preference.PreferenceManager;
 public class Account {
     private String user_id = null;
     private String password = null;
+    private boolean loaded = false;
 
     /**
-     * SharedPreferenceに保存されたデータを読み取ってAccountを作る
+     * Create Account instance
+     */
+    public Account() {
+    }
+
+    /**
+     * create Account instance with Context and load
      * 
      * @param context
-     * @return Accountのインスタンス
      * @throws BlankException
      *             アカウント情報が設定されていない場合
      */
-    public static Account load(Context context) throws BlankException {
-        SharedPreferences pref = PreferenceManager
-                .getDefaultSharedPreferences(context);
-        String user_id = pref.getString("account", null);
-        String password = pref.getString("password", null);
-        if (user_id == null || password == null || user_id == ""
-                || password == "") {
-            throw new BlankException();
-        }
-        return new Account(user_id, password);
-    }
-
-    private Account(String user_id, String password) {
-        this.user_id = user_id;
-        this.password = password;
+    public Account(Context context) throws BlankException {
+        this.load(context);
     }
 
     /**
-     * ユーザ名を取得
+     * load account information from SharedPreference
      * 
-     * @return
+     * @param context
+     * @return success?
+     * @throws BlankException
+     */
+    public boolean load(Context context) throws BlankException {
+        SharedPreferences pref = PreferenceManager
+                .getDefaultSharedPreferences(context);
+        user_id = pref.getString("account", null);
+        password = pref.getString("password", null);
+        if (user_id == null || password == null || user_id == ""
+                || password == "") {
+            throw new BlankException();
+
+        }
+        loaded = true;
+        return true;
+    }
+
+    /**
+     * get user_id
+     * 
+     * @return user_id
      */
     public String getUser() {
         return this.user_id;
     }
 
     /**
-     * パスワードを取得
+     * get password
      * 
-     * @return
+     * @return password
      */
     public String getPassword() {
         return this.password;
