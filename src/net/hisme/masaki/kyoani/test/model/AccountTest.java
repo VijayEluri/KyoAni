50377888c88a4e4065f88fc4f8ec926e224e3526
Yamada Masaki <masaki@hisme.net>
2010-12-13 02:09:49 +0900

    add blank test

---
+++ src/net/hisme/masaki/kyoani/test/model/AccountTest.java
@@ -0,0 +1,9 @@
+package net.hisme.masaki.kyoani.test.model;
+
+import net.hisme.masaki.kyoani.models.Account;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class AccountTest {
+
+}
